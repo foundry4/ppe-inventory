b68ba03931281cf9db9fb61f5f3d201cdeabc787
David Carboni <david@carboni.io>
2020-03-21 11:49:52 +0000

    Initial post and publish to pubsub.

---
+++ form/index.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const {PubSub} = require('@google-cloud/pubsub')
+
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+exports.form = (req, res) => {
+  if (req.method === 'POST') {
+
+    // Data to publish
+    let message = JSON.stringify(req.body)
+
+    // Send message
+    // TODO dump to the console in case of error
+    const pubSubClient = new PubSub();
+    const dataBuffer = Buffer.from(data);
+    const customAttributes = {'timestamp': Date.now()}
+    const messageId = pubSubClient
+      .topic('form-submissions')
+      .publish(dataBuffer, customAttributes)
+    console.log(`Message ${messageId} published.`)
+    res.status(200).send(`POST: ${message} -> ${messageId}\n`)
+  } else {
+    res.status(200).send('GET: Hello World!')
+  }
+};
