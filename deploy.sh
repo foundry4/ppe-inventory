1d5c4cc0ad2fd0a260d0e742d0ee0c18e367e750
David Carboni <david@carboni.io>
2020-03-21 12:58:10 +0000

    Simplified by using default sevice account, rather than creating a dedicated one.

--- deploy.sh
+++ deploy.sh
@@ -37,30 +37,30 @@ gcloud config set project $project
 sheet_id=$(cat sheet-id.txt)
 worksheet_name=$(cat worksheet-name.txt)
 
-# Service account
+# Service account - less privilege, but more complex
 
-service_account_name=functions
-gcloud iam service-accounts describe ${service_account_name}@${project}.iam.gserviceaccount.com || \
-gcloud iam service-accounts create ${service_account_name} --display-name "Funcitons" --description "Service accout for spreadform functions"
-service_account=${service_account_name}@${project}.iam.gserviceaccount.com
+#service_account_name=functions
+#gcloud iam service-accounts describe ${service_account_name}@${project}.iam.gserviceaccount.com || \
+#gcloud iam service-accounts create ${service_account_name} --display-name "Funcitons" --description "Service accout for spreadform functions"
+#service_account=${service_account_name}@${project}.iam.gserviceaccount.com
 
 # Pubsub topic
 
 gcloud pubsub topics describe form-submissions || \
 gcloud pubsub topics create form-submissions
-echo "{
-     \"bindings\": [
-       {
-         \"role\": \"roles/pubsub.editor\",
-         \"members\": [
-           \"serviceAccount:${service_account}\"
-         ]
-       }
-     ]
-   }" > form-submissions.policy.json
+#echo "{
+#     \"bindings\": [
+#       {
+#         \"role\": \"roles/pubsub.editor\",
+#         \"members\": [
+#           \"serviceAccount:${service_account}\"
+#         ]
+#       }
+#     ]
+#   }" > form-submissions.policy.json
 # Answer yes to the prompt to update the policy:
-printf 'y\n' | gcloud pubsub topics set-iam-policy projects/spreadform/topics/form-submissions form-submissions.policy.json
-rm form-submissions.policy.json 
+#printf 'y\n' | gcloud pubsub topics set-iam-policy projects/spreadform/topics/form-submissions form-submissions.policy.json
+#rm form-submissions.policy.json 
 
 # Pubsub subscriptions
 
@@ -73,4 +73,4 @@ cd $base/form
 env_vars="--set-env-vars=[SHEET_ID=$sheet_id,WORKSHEET_NAME=$worksheet_name]"
 options="--region=europe-west2 --memory=256MB --trigger-http --allow-unauthenticated"
 
-gcloud functions deploy form --runtime=nodejs10 ${env_vars} ${options} --entry-point=form --service-account=${service_account}
+gcloud functions deploy form --runtime=nodejs10 ${env_vars} ${options} --entry-point=form #--service-account=${service_account}
