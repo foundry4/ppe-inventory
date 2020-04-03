f7b18297f55cb671b6d40eb410730adb0a031e24
David Carboni <david@carboni.io>
2020-04-03 20:13:58 +0100

    Getting registration going.

--- deploy.sh
+++ deploy.sh
@@ -46,33 +46,6 @@ worksheet_name=$(cat worksheet-name.txt)
 
   gcloud services enable sheets.googleapis.com
 
-# Service account - less privilege, but more complex
-
-#service_account_name=functions
-#gcloud iam service-accounts describe ${service_account_name}@${project_id}.iam.gserviceaccount.com || \
-#gcloud iam service-accounts create ${service_account_name} --display-name "Funcitons" --description "Service accout for spreadform functions"
-#service_account=${service_account_name}@${project_id}.iam.gserviceaccount.com
-
-# Pubsub topic
-
-#gcloud pubsub topics describe form-submissions || \
-#gcloud pubsub topics create form-submissions
-#echo "{
-#     \"bindings\": [
-#       {
-#         \"role\": \"roles/pubsub.editor\",
-#         \"members\": [
-#           \"serviceAccount:${service_account}\"
-#         ]
-#       }
-#     ]
-#   }" > form-submissions.policy.json
-# Answer yes to the prompt to update the policy:
-#printf 'y\n' | gcloud pubsub topics set-iam-policy projects/spreadform/topics/form-submissions form-submissions.policy.json
-#rm form-submissions.policy.json 
-
-# Google sheets function
-# NB: limited to 1 instance to avoid race conditions when updating the spreadsheet
 
 # Static content
 #gsutil mb gs://ppe-inventory
@@ -84,29 +57,40 @@ gsutil iam ch allUsers:objectViewer gs://ppe-inventory
 # Web form function
 
 cd $base/form
+env_vars="--set-env-vars=DOMAIN=europe-west2-${project_id}.cloudfunctions.net"
 options="--region=europe-west2 --memory=256MB --allow-unauthenticated"
 
-gcloud functions deploy form --runtime=python37 --trigger-http ${options} #--service-account=${service_account}
+gcloud functions deploy form --runtime=python37 --trigger-http ${env_vars} ${options}
+
+
+# Registration function
+
+cd $base/register
+env_vars="--set-env-vars=DOMAIN=europe-west2-${project_id}.cloudfunctions.net"
+options="--region=europe-west2 --memory=256MB --allow-unauthenticated"
+
+gcloud functions deploy register --runtime=python37 --trigger-http ${env_vars} ${options}
 
 
 # Data update function
 
 cd $base/update
-env_vars="--set-env-vars=FORM_PAGE=$form_page,SUCCESS_PAGE=$success_page,ERROR_PAGE=$error_page"
+env_vars="--set-env-vars=DOMAIN=europe-west2-${project_id}.cloudfunctions.net"
 options="--region=europe-west2 --memory=256MB --allow-unauthenticated"
 
-gcloud functions deploy form --runtime=nodejs10 --trigger-http ${env_vars} ${options} #--service-account=${service_account}
+gcloud functions deploy form --runtime=nodejs10 --trigger-http ${env_vars} ${options}
 
 
 # Google sheets function
+# NB: limited to 1 instance to avoid race conditions when updating the spreadsheet
 
 cd $base/sheets
 concurrency=" --max-instances=1"
 env_vars="--set-env-vars=SHEET_ID=$sheet_id,WORKSHEET_NAME=$worksheet_name"
 options="--region=europe-west2 --memory=256MB --allow-unauthenticated"
 
-gcloud functions deploy inventory --runtime=python37 --trigger-http ${concurrency} ${env_vars} ${options} #--service-account=${service_account}
-gcloud functions deploy sheets --runtime=python37 --trigger-topic=form-submissions ${concurrency} ${env_vars} ${options} #--service-account=${service_account}
+gcloud functions deploy inventory --runtime=python37 --trigger-http ${concurrency} ${env_vars} ${options}
+gcloud functions deploy sheets --runtime=python37 --trigger-topic=form-submissions ${concurrency} ${env_vars} ${options}
 
 cd $base
 
--- form/main.py
+++ form/main.py
@@ -4,6 +4,8 @@ from flask import request, make_response, redirect, render_template, abort
 def form(request):
 
     site = request.cookies.get('site')
+    code = request.cookies.get('code')
+
     template = 'ppe-inventory.html' if site else 'ppe-error.html'
 
     form = make_response(render_template(template, 
