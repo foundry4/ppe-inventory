a2ca86f0beebc7b599aed686884baca412570ec3
David Carboni <david@carboni.io>
2020-04-03 23:15:37 +0100

    Cleaning out files.

--- curl.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Exit on error
-set -euxo pipefail
-
-DATE=$(date +"%Y%m%d%H%M")
-curl -X POST -d "name=David&company=Carboni.io&submitted-at=$DATE" https://europe-west2-loans-271821.cloudfunctions.net/form
