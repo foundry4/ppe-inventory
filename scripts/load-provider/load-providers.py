aa2a729cb306dd1d2116db6d88d2e337e2f07de4
Frank Phillips <frank@dodion.co.uk>
2020-04-16 14:23:05 +0100

    Initial changes to support file outputs for links and duplicates

--- scripts/load-provider/load-providers.py
+++ scripts/load-provider/load-providers.py
@@ -5,35 +5,90 @@
 #
 #           $ export GOOGLE_APPLICATION_CREDENTIALS="ppe-inventory-dev.json"
 #
+
 from google.cloud import datastore
+import urllib
 import csv
 import uuid
-# Instantiates a client
-datastore_client = datastore.Client()
+import pprint
+import sys
 
-# The kind for the new entity
-kind = 'Site'
+baseUrl = 'https://europe-west2-ppe-inventory-273009.cloudfunctions.net'
+if len(sys.argv) > 1:
+    baseUrl = sys.argv[1]
+    print(f'Link base url is {baseUrl}')
+
+# Read input from csv file
+
+provider_dictionary = {}
+duplicate_providers_list = []
 
 with open('providers.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+
     for row in reader:
-        provider = row[1]
+        provider = row[1].strip()
         email = row[10]
         address = row[12]
         code = str(uuid.uuid4())
-        print(f'provider => {provider}   uuid => {code}   email => {email}   address => {address}')
+        name = urllib.parse.quote_plus(provider)
+        link = baseUrl + '/register?name=' + name + '&code=' + code
 
-        # The name/ID for the new entity
-        name = provider
+        if name in provider_dictionary.keys():
+            print(f'Duplicate found for code provider {provider}')
+            duplicate_providers_list.append(
+                {"code": provider_dictionary[name]['code'], "provider": provider_dictionary[name]['provider'],
+                 "email": provider_dictionary[name]['email'], "address": provider_dictionary[name]['address']})
 
-        # The Cloud Datastore key for the new entity
-        task_key = datastore_client.key(kind, name)
+        provider_dictionary[name] = {
+            "provider": provider,
+            "code": code,
+            "email": email,
+            "address": address,
+            "link": link
+        }
 
-        # Prepares the new entity
-        task = datastore.Entity(key=task_key)
-        task['code'] = code
-        task['email'] = email
-        task['address'] = address
+# Write duplicates to file
+f = open("duplicates.csv", "w")
+for duplicate in duplicate_providers_list:
+    pprint.pprint(duplicate)
+    f.write(
+        duplicate.get('code') + ',' + duplicate.get('provider') + ',' + duplicate.get('email') + ',' + duplicate.get(
+            'address') + '\n')
+f.close()
 
-        # Saves the entity
-        datastore_client.put(task)
+# Write links to file
+f = open("links.csv", "w")
+
+# Write to datastore
+
+# Identify the 'kind' of entity
+kind = 'Site'
+
+# Instantiates a client
+datastore_client = datastore.Client()
+
+for key, value in provider_dictionary.items():
+    print(key)
+    pprint.pprint(value)
+    provider = value.get('provider')
+    code = value.get('code')
+    email = provider_dictionary[key]['email']
+    address = provider_dictionary[key]['address']
+
+    # The Cloud Datastore key for the new entity
+    task_key = datastore_client.key(kind, key)
+
+    # Prepares the new entity
+    task = datastore.Entity(key=task_key)
+    task['provider'] = value.get('provider')
+    task['code'] = value.get('code')
+    task['email'] = value.get('email')
+    task['address'] = value.get('address')
+
+    # Saves the entity
+    datastore_client.put(task)
+
+    f.write(value.get('provider') + ',' + value.get('email') + ',' + value.get('link') + '\n')
+
+f.close()
