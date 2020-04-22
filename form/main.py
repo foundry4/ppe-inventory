6a6f5e57f177017a999757bd9917ff677950e12e
Frank Phillips <frank@dodion.co.uk>
2020-04-22 16:03:42 +0100

    Log site data to confirm structure inside form function

--- form/main.py
+++ form/main.py
@@ -177,7 +177,7 @@ def get_sheet_data(site):
 
     for field in fields:
         if site[field]:
-            field: site[field]
+            ss[field] = site[field]
 
     # ss.update({
     #     'site': site['site'],
