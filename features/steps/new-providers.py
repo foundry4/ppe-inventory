062502baf97e2de5ed64f4dc802e63cd7c448d5a
Frank Phillips <frank@dodion.co.uk>
2020-04-28 22:09:44 +0100

    Script to create/update new sites from spreadsheet

--- features/steps/new-providers.py
+++ features/steps/new-providers.py
@@ -2,62 +2,79 @@ from behave import *
 from google.cloud import datastore
 import os
 import uuid
+import pandas as pd
 
 
-@given('provider "{provider}" exists')
-def step_impl(context, provider):
-    print(u'STEP: Given provider Provider One exists')
-    context.provider_one = provider
+@given('site "{site}" exists')
+def step_impl(context, site):
+    print(f'STEP: Given provider {site} exists')
+    context.domain = os.getenv('DOMAIN')
     # Instantiates a client
     datastore_client = datastore.Client()
-    provider_key = datastore_client.key('Site', context.provider_one)
+    context.site_one = site
+    provider_key = datastore_client.key('Site', context.site_one)
     datastore_client.delete(provider_key)
     entity = datastore.Entity(key=provider_key)
-    entity['provider'] = context.provider_one
-    entity['code'] = str(uuid.uuid4())
+    entity['site'] = context.site_one
+    code = str(uuid.uuid4())
+    entity['code'] = code
+    context.site_one_link = 'https://' + context.domain + '/register?site=' + site + '&code=' + code
+    entity['link'] = context.site_one_link
     datastore_client.put(entity)
 
 
-@step('provider "{provider}" does not exist')
-def step_impl(context, provider):
-    print(u'STEP: And provider Provider Two does not exists')
-    context.provider_two = provider
+@step('site "{site}" does not exist')
+def step_impl(context, site):
+    print(f'STEP: And site {site} does not exists')
+    context.provider_two = site
     # Instantiates a client
     datastore_client = datastore.Client()
-    datastore_client.delete(datastore_client.key('Site', provider))
+    datastore_client.delete(datastore_client.key('Site', site))
 
 
-@step("both providers are included in the input file")
+@step("both sites are included in the input file")
 def step_impl(context):
-    print(u'STEP: And both providers are included in the input file')
-    f = open("new-providers.csv", "w+")
-    f.write('provider,borough,contact_name,contact_email,telephone,service_type,location,postcode\n')
-    f.write(
-        f'{context.provider_one},borough1,contact_name1,contact_email1,telephone1,service_type1,location1,postcode1\n')
-    f.write(
-        f'{context.provider_two},borough2,contact_name2,contact_email2,telephone2,service_type2,location2,postcode2\n')
-    f.close()
+    context.file = 'features/resources/input-file.xlsx'
+    print(f'STEP: And both sites are included in the input file at {context.file}')
 
 
 @when("the input file is processed")
 def step_impl(context):
     print(u'STEP: When the input file is processed')
-    os.system('pwd')
-    command = 'python3 scripts/new-providers/new-providers.py ' + str(os.getenv('DOMAIN')) + ' ' + str(os.getenv('INPUT_FILE'))
+    context.output_file = 'features/resources/output-file.xlsx'
+    command = f'python3 scripts/new-providers/new-providers.py {context.domain} {context.file} {context.output_file}'
     print(command)
     os.system(command)
 
 
-@step('"{provider}" is updated with the original link')
-def step_impl(context, provider):
-    print(u'STEP: And Test Provider Two is updated')
-
-
-@then('"{provider}" is created with a new link')
-def step_impl(context, provider):
-    print(f'STEP: Then {provider} is created')
+@then('site "{site}" is updated with the original link')
+def step_impl(context, site):
+    print(f'STEP: And site {site} is updated with the original link')
     # Instantiates a client
     datastore_client = datastore.Client()
-    key = datastore_client.key('Site', provider)
-    p = datastore_client.get(key)
-    print(p)
+    key = datastore_client.key('Site', site)
+    assert key is not None
+    entity = datastore_client.get(key)
+    assert entity['link'] == context.site_one_link
+    print(entity)
+
+
+@then('site "{site}" is created with a new link')
+def step_impl(context, site):
+    print(f'STEP: Then {site} is created')
+    # Instantiates a client
+    datastore_client = datastore.Client()
+    key = datastore_client.key('Site', site)
+    assert key is not None
+    entity = datastore_client.get(key)
+    assert entity['link'] == 'https://' + context.domain + '/register?site=' + site + '&code=' + entity['code']
+    print(entity)
+
+
+@step('site "{site}" appears in the output file as "{status}"')
+def step_impl(context, site, status):
+    print(f'STEP: And site {site} appears in the output file as {status}')
+    df = pd.read_excel(context.output_file)
+    row = df.loc[df['site'] == site]
+    print(row)
+    assert row['comment'].values[0] == status
--- scripts/new-providers/new-providers.py
+++ scripts/new-providers/new-providers.py
@@ -1,15 +1,16 @@
 #
 # This script is intended to add new providers and update existing providers in the datastore from an Excel input file.
 #
-# Pass the PROD, TEST or DEV base url as the first commandline argument and the the input file as the second
-# argument e.g.
+# Pass the PROD, TEST or DEV base url as the first commandline argument and the input file as the second
+# argument. A third argument identifying the out put file is optional. If this is not provided then the output file
+# will tbe the timestamp appended with 'new-providers.xlsx'. For example:
 #
-#   $ python3 new-providers.py https://********.cloudfunctions.net "North East Primary Care Contact List v(8) (8).xlsx"
+#   $ python3 new-providers.py https://********.cloudfunctions.net "input-file.xlsx" "output-file.xlsx"
 #
 # An attempt is made to create a new record for each of the entries in the input file.
 # If the provider already exists then the record is updated with the values from the input file
 # but the code value is not changed so that the provider's link remains valid. In all cases the results
-# and the links for the providers are appended to the output file.
+# and the links for the providers are appended to the output Excel file.
 #
 # The script will try to use the credentials of a locally configured service account so it
 # is important to provide the key file as an environment variable e.g.
@@ -19,130 +20,168 @@
 
 
 from google.cloud import datastore
-import csv
 import uuid
 import sys
 import logging
-from notifications_python_client.notifications import NotificationsAPIClient
-import os
 import pandas as pd
+import urllib.parse
+from email_validator import validate_email, EmailNotValidError
+import datetime
 
 baseUrl = ''
 if len(sys.argv) > 2:
     baseUrl = sys.argv[1]
     input_file = sys.argv[2]
 else:
-    print('Two arguments are required i.e. the base url for the target environment and then the input file.')
+    print('Missing arguments i.e. first is the base url for the target environment and second is the input file.')
     sys.exit(1)
-
-logfile = "new-providers.log"
+now = datetime.datetime.now()
+logfile = f'{now} new-providers.log'
 logging.basicConfig(level=logging.INFO, filename=logfile)
+print(f'Writing logs to "{logfile}" file ...')
+logging.info(f'Base url is {baseUrl}')
+logging.info(f'Input file is {input_file}')
+if len(sys.argv) > 3:
+    output_file = sys.argv[3]
+else:
+    output_file = f'{now} new-providers.xlsx'
+sheet_in = pd.read_excel(input_file)
+sheet_out = pd.ExcelWriter(output_file)
 
-print(f'logging to {logfile} ...')
-logging.info(f'Link base url is {baseUrl}')
+df = pd.DataFrame(
+    columns=['provider', 'code', 'link', 'site', 'acute', 'location', 'postcode', 'service_type', 'borough',
+             'contact_name_1', 'contact_name_2', 'email_1', 'email_2', 'email_3', 'telephone',
+             'pcn_network', 'practice_code', 'parent', 'parent_link', 'comment'])
 
-# Read from input file
-logging.info('Reading records from input file...')
-input_spreadsheet = pd.read_excel(input_file)
-print(input_spreadsheet.head)
-
-print(input_spreadsheet.columns.ravel())
+logging.info(sheet_in.head)
+logging.info(sheet_in.columns.ravel())
 
 # Instantiates a client
 datastore_client = datastore.Client()
 
-f = open(input_file + "-output.csv", "w+")
 
-ProviderObjects = {}
+# Helper functions to clean up input values
+def get_clean_value(value):
+    output = ''
+    if pd.notnull(value):
+        output = str(value).strip()
+    return output
 
-for index, row in input_spreadsheet.iterrows():
 
-    provider = str(str(row['Practice_Name']) + " " + str(row['Post_Code'])).replace("&", "and").strip()  # Both required to produce unique key
-    acute = "no"  # I think in this case
-    organisation = ""
-    location = str(str(row['Address_Line_1']) + " " + str(row['Address_Line_2']) + " " + str(row['Address_Line_3'])).strip()  # Could be improved
-    postcode = str(row['Post_Code']).upper().strip()
-    service_type = ""  # Not in the input file but was in the the first version of input file
-    borough = ""  # Not in the input file but was in the the first version of input file
-    contact_name_1 = str(row['Practice_Manager_Name']).strip()
-    contact_name_2 = str(row['Lead_GP_Name/GP Principals']).strip()
-    contact_name_3 = ""  # Not in the input file but they have provided 3 emails
-    email_1 = str(row['Practice_Manager_email']).strip()
-    email_2 = str(row['Lead_GP_email']).strip()
-    email_3 = str(row['Alternative_Practice_Email address']).strip()
-    telephone_1 = str(row['Practice_Phone_Number_\n(Bypass)']).strip()
-    telephone_2 = str(row['Practice_Phone_Number_(Public)']).strip()
-    telephone_3 = ""  # Not in the input file but they have provided 3 emails
-    # Are these GP specific fields?
-    ccg = str(row['CCG']).strip()
-    ods_code = str(row['ODS Code']).strip()
-    cont_type = str(row['Cont Type']).strip()
-    pcn_network = str(row['PCN Network']).strip()
-    practice_code = str(row['Practice Code']).strip()
-    practice_type = str(row['Branch/\nGP Led/\nWalk in Centre']).strip()
+def get_provider(practice_name, postcode_input):
+    provider_output = ''
+    if pd.notnull(practice_name):
+        provider_output = practice_name.strip()
+    if pd.notnull(postcode_input):
+        provider_output = str(provider_output + ' ' + postcode_input).strip()
+    return provider_output
+
+
+def get_site(practice_name, postcode_input):
+    return get_provider(practice_name, postcode_input).replace("&", "and").strip()
+
+
+def get_location(line1, line2, line3):
+    location_str = ''
+    if pd.notnull(line1):
+        location_str = line1
+    if pd.notnull(line2):
+        location_str = location_str + ', ' + line2
+    if pd.notnull(line3):
+        location_str = location_str + ', ' + line3
+    return location_str.strip()
+
+
+def get_borough(ccg_input):
+    borough_output = ''
+    if pd.notnull(ccg_input):
+        borough_output = str(ccg_input).strip()[4:-4]
+    return borough_output
+
+
+def get_email(email_input):
+    email = ''
+    if pd.notnull(email_input):
+        email = email_input.replace(";", " ").replace("'", " ").replace("\n", " ").strip().split(' ', 1)[0]
+        try:
+            v = validate_email(email)
+            email = v["email"].lower()
+        except EmailNotValidError as e:
+            logging.error(f'{str(e)}  [{email}] from [{email_input}]')
+    return email
+
+
+# Iterate over input file to calculate new values
+for index, row in sheet_in.iterrows():
+    provider = get_provider(row['Practice_Name'], row['Post_Code'])
+    site = get_site(row['Practice_Name'], row['Post_Code'])
+    acute = "no"
+    location = get_location(row['Address_Line_1'], row['Address_Line_2'], row['Address_Line_3'])
+    postcode = get_clean_value(row['Post_Code']).upper()
+    service_type = "Primary Care - GP Federation"
+    borough = get_borough(row['CCG'])
+    contact_name_1 = get_clean_value(row['Practice_Manager_Name'])
+    contact_name_2 = get_clean_value(row['Lead_GP_Name/GP Principals'])
+    email_1 = get_email(row['Practice_Manager_email'])
+    email_2 = get_email(row['Lead_GP_email'])
+    email_3 = get_email(row['Alternative_Practice_Email address'])
+    telephone = get_clean_value(row['Practice_Phone_Number_\n(Bypass)'])
+    pcn_network = get_clean_value(row['PCN Network'])
+    practice_code = get_clean_value(row['Practice Code'])
+    parent = ""
+    if parent != "":
+        parent_link = 'https://' + baseUrl + '/children?parent=' + urllib.parse.quote(str(parent))
+    else:
+        parent_link = ""
 
     # Check if record exists so can use existing code otherwise generate new code value
-    existing_provider = datastore_client.get(datastore_client.key('Site', provider))
+    provider_key = datastore_client.key('Site', site)
+    existing_provider = datastore_client.get(provider_key)
     if existing_provider:
-        comment = 'EXISTING'
+        comment = 'UPDATED'
         code = existing_provider['code']
+        link = existing_provider['link']
+        entity = existing_provider
     else:
-        comment = 'NEW     '
+        comment = 'CREATED'
         code = str(uuid.uuid4())
+        link = 'https://' + baseUrl + '/register?site=' + site + '&code=' + code
+        entity = datastore.Entity(key=provider_key)
 
-    # Prepare to create or update the entity
-    provider_key = datastore_client.key('Site', provider)
-    entity = datastore.Entity(key=provider_key)
+    # Update the entity values
     entity['code'] = code
+    entity['site'] = site
     entity['provider'] = provider
     entity['acute'] = acute
-    entity['organisation'] = organisation
     entity['location'] = location
     entity['postcode'] = postcode
     entity['service_type'] = service_type
     entity['borough'] = borough
     entity['contact_name_1'] = contact_name_1
     entity['contact_name_2'] = contact_name_2
-    entity['contact_name_3'] = contact_name_3
     entity['email_1'] = email_1
     entity['email_2'] = email_2
     entity['email_3'] = email_3
-    entity['telephone_1'] = telephone_1
-    entity['telephone_2'] = telephone_2
-    entity['telephone_3'] = telephone_3
-    entity['ccg'] = ccg
-    entity['ods_code'] = ods_code
-    entity['cont_type'] = cont_type
+    entity['telephone'] = telephone
     entity['pcn_network'] = pcn_network
     entity['practice_code'] = practice_code
-    entity['practice_type'] = practice_type
+    entity['parent'] = parent
+    entity['parent_link'] = parent_link
+    entity['link'] = link
 
-
-
-    # Saves the entity
+    # Save the entity
+    logging.info(entity)
     datastore_client.put(entity)
 
-    link = baseUrl + '/register?site=' + provider + '&code=' + code
-    link = '{0: <200}'.format(link)
-    line = f'{comment}, {link} \n'
-    logging.info(line)
-    f.write(line)
+    # Write values to output data frame
+    new_row = {'provider': provider, 'code': code, 'link': link, 'site': site, 'acute': acute, 'location': location,
+               'postcode': postcode, 'service_type': service_type, 'borough': borough, 'contact_name_1': contact_name_1,
+               'contact_name_2': contact_name_2, 'email_1': email_1, 'email_2': email_2, 'email_3': email_3,
+               'telephone': telephone, 'pcn_network': pcn_network,
+               'practice_code': practice_code, 'parent': parent, 'parent_link': parent_link, 'comment': comment}
+    df = df.append(new_row, ignore_index=True)
 
-f.close()
-
-notifications_client = NotificationsAPIClient(os.getenv('EMAIL_API_KEY'))
-
-response = notifications_client.get_template(
-    'a9b20e19-efe9-4e6a-b081-67b549e596dc'  # required string - template ID
-)
-
-print(response)
-
-response = notifications_client.send_email_notification(
-    email_address='frank@notbinary.co.uk',  # required string
-    template_id='a9b20e19-efe9-4e6a-b081-67b549e596dc',  # required UUID string
-)
-
-print(response)
-
-
+# Write data frame to output file
+print(f'Saving output file to "{output_file}"...')
+df.to_excel(sheet_out)
+sheet_out.save()
