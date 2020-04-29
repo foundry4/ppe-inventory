9c369261c12c2f0bcd03b4b3a984348d62397d61
Frank Phillips <frank@dodion.co.uk>
2020-04-29 16:17:07 +0100

    Experimenting with search feature

--- search/main.py
+++ search/main.py
@@ -7,27 +7,38 @@ import os
 
 currentTime = datetime.datetime.now()
 
+LINKS_SEARCH = 'links'
+PARENT_SEARCH = 'parent'
+
 
 def search(request):
     print(request)
 
     request_args = request.args
-    borough = ''
-    pcn = ''
-    service_type = ''
+    search_type = 'Invalid search type'
+    result_label = 'No results'
 
-    if request_args:
-        borough = request_args['borough']
-        pcn = request_args['pcn']
-        service_type = request_args['service_type']
     # Instantiates a client
     datastore_client = datastore.Client()
     query = datastore_client.query(kind='Site')
-    query.add_filter('borough', '=', borough)
-    query.add_filter('pcn', '=', pcn)
-    query.add_filter('service_type', '=', service_type)
-    # query.order = ['site']
-    # query = datastore_client.query()
+
+    if request_args:
+        if request_args['search_type'] == LINKS_SEARCH:
+            search_type = 'Search for community sites'
+            borough = request_args['borough']
+            pcn = request_args['pcn']
+            service_type = request_args['service_type']
+            results_label = f'Results using filter values  Borough: {borough}  PCN: {pcn}  Service Type: {service_type}'
+            query.add_filter('borough', '=', borough)
+            query.add_filter('pcn', '=', pcn)
+            query.add_filter('service_type', '=', service_type)
+            results = list(query.fetch())
+        if request_args['search_type'] == PARENT_SEARCH:
+            search_type = 'Search for child sites'
+            parent = request_args['parent']
+            result_label = f'Results using filter values Parent: {parent}'
+            query.add_filter('parent', '=', parent)
+            results = list(query.fetch())
     results = list(query.fetch())
     print(results)
     sites = []
@@ -35,4 +46,8 @@ def search(request):
         print(site['site'])
         sites.append(site['link'])
 
-    return render_template('results.html', sites=results, assets='https://storage.googleapis.com/ppe-inventory')
+    return render_template('results.html',
+                           sites=results,
+                           assets='https://storage.googleapis.com/ppe-inventory',
+                           search_type=search_type,
+                           results_label=result_label)
