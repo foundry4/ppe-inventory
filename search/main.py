3549233eb5236e190705661b858f5caeec755cb8
Frank Phillips <frank@dodion.co.uk>
2020-04-29 13:41:10 +0100

    Experiment with search function

--- search/main.py
+++ search/main.py
@@ -9,97 +9,18 @@ currentTime = datetime.datetime.now()
 
 
 def search(request):
-    sites = {'Royal London Hospital': 'pr234ted',
-             'Whipps Cross Hospital': 'el324os',
-             'St Bartholomew': 'ak907atp',
-             'Mile End Hospital': 'ap193fw',
-             'Newham Hospital': 'th738go',
-             'Nightingale Hospital': 'b047a27a-e6be-4a03-8dc9-2123495a3c09'}
+    search_type = request.args.get('search_type')
 
-    post = False
-    site = None
-    landing = request.args.get('landing')
-    name = request.args.get('site')
-    code = request.args.get('code')
     client = datastore.Client()
-    print(f'landing:{landing};name:{name};code:{code}')
-
-    if name and code:
-        site = get_site(name, code, client)
-    if site and request.method == 'POST':
-        print("data are being updated.")
-        update_site(site, client, request, code)
-        publish_update(site)
-        post = True
-
-    if landing == 'true':
-        print('Landing == true')
-        template = 'barts.html'
-
-    else:
-        print('Landing != true')
-        site = get_site(name, code, client)
-        template = 'success.html' if post else 'form.html' if site else 'error.html'
 
     # Construct a full URL to redirect to
     # otherwise we seem to end up on http
     domain = os.getenv('DOMAIN')
-    form_action = f'https://{domain}/barts?site={name}&code={code}'
-    landing_page = f'https://{domain}/barts?landing=true'
-
-    print(f"Rendering {template}")
 
     response = make_response(render_template('results.htm',
-                                             site=site,
-                                             sites=sites,
-                                             form_action=form_action,
-                                             landingPage=landing_page,
                                              currentTime=datetime.datetime.now().strftime('%H:%M %d %B %y'),
                                              assets='https://storage.googleapis.com/ppe-inventory',
                                              data={}
                                              ))
 
     return response
-
-
-def get_site(name, code, client):
-    print(f"Getting site: {name}/{code}")
-    key = client.key('Site', name)
-    site = client.get(key)
-    if site and site.get('code') == code:
-        return site
-    return None
-
-
-def update_site(site, client, request, code):
-    acute = site.get('acute')
-    print(f"Updating site: {site}/{code}")
-    # Update the site
-    site.update(request.form)
-
-    # Values not to change
-    site['site'] = site.key.name
-    site['acute'] = acute
-    site['code'] = code
-
-    print(f"Updating site {site}")
-    client.put(site)
-
-
-def publish_update(site):
-    # Publish a message to update the Google Sheet:
-
-    message = {}
-    message.update(site)
-    message['last_update'] = (datetime.datetime.now() + datetime.timedelta(hours=1)).strftime('%H:%M %d %B %y')
-
-    publisher = pubsub_v1.PublisherClient()
-
-    project_id = os.getenv("PROJECT_ID")
-    topic_path = publisher.topic_path(project_id, 'form-submissions')
-
-    data = json.dumps(message).encode("utf-8")
-
-    future = publisher.publish(topic_path, data=data)
-
-    print(f"Published update to site {site.key.name}: {future.result()}")
