99decd4ed2d3dfe630be22b4b2113d4ca6a6bd22
Frank Phillips <frank@dodion.co.uk>
2020-05-04 21:35:19 +0100

    Experiment with Portal in DEV

--- features/environment.py
+++ features/environment.py
@@ -6,7 +6,7 @@ logging.basicConfig(level=logging.INFO, filename="tests.log")
 
 
 def before_all(context):
-    context.base_url = "https://" + os.getenv('DOMAIN')
+    context.base_url = "https://" + os.getenv('PORTAL')
     context.valid_provider_name = os.getenv('VALID_PROVIDER_NAME')
     context.valid_provider_code = os.getenv('VALID_PROVIDER_CODE')
     context.valid_link = context.base_url + "/register?site=" + str(context.valid_provider_name) + "&code=" + str(
