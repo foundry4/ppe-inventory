c220a380dda8546cb3825b217e8b6e6102ad195b
Simon <mail@simon-neumann.com>
2020-05-20 15:36:15 +0100

    Change Apron to Aprons

--- dashboard/main.py
+++ dashboard/main.py
@@ -49,7 +49,7 @@ def get_item_names():
                   'gloves': 'Gloves',
                   'gowns': 'Gowns',
                   'hand-hygiene': 'Hand Hygiene',
-                  'apron': 'Apron'}
+                  'apron': 'Aprons'}
     return item_names
 
 
